efc55e0715c13b0131c75e460c8dde02cc451ab0
nov11 <529079634@qq.com>
2021-12-25 23:50:36 +0800

    init

---
+++ src/main/java/pkg/ModelLoader.java
@@ -0,0 +1,53 @@
+package pkg;
+
+import org.slf4j.Logger;
+import org.tensorflow.Graph;
+import org.tensorflow.SavedModelBundle;
+import org.tensorflow.Tensor;
+import org.tensorflow.TensorFlow;
+import org.tensorflow.ndarray.Shape;
+import org.tensorflow.ndarray.StdArrays;
+import org.tensorflow.types.TInt32;
+
+import java.util.Arrays;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class ModelLoader {
+    private static final Logger logger = getLogger(ModelLoader.class);
+    private final String modelPath;
+    private final String tag;
+
+    static {
+        TensorFlow.loadLibrary("binary/_cuckoo_hashtable_ops.so");
+    }
+
+    public ModelLoader(String path, String tag) {
+        this.modelPath = path;
+        this.tag = tag;
+    }
+
+    public void load() {
+        SavedModelBundle savedModelBundle = SavedModelBundle.load(this.modelPath, this.tag);
+
+        int pa[][] = new int[1][512];
+        Arrays.fill(pa[0], 1);
+
+        TInt32 tensorPA = TInt32.tensorOf(Shape.of(1, 512));
+        StdArrays.copyTo(pa, tensorPA);
+        Tensor tensor = savedModelBundle.session().runner()
+                .feed("pa", tensorPA)
+                .feed("pb", tensorPA)
+                .fetch("Add:0")
+                .run().get(0);
+
+        logger.info("ret : {}", tensor.asRawTensor());
+    }
+
+    public static void main(String[] args) {
+        String p = args[0];
+        String tag = args[1];
+        ModelLoader modelLoader = new ModelLoader(p, tag);
+        modelLoader.load();
+    }
+}
